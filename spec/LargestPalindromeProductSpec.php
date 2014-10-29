43976d79958a33f86f9bcfa20aaf9bb6cdd90eb3
Dimitrij P <d.phoursa@outlook.com>
2014-10-29 12:24:58 +0000

    Added description to problem 4

--- spec/LargestPalindromeProductSpec.php
+++ spec/LargestPalindromeProductSpec.php
@@ -5,6 +5,13 @@ namespace spec;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * Problem 4
+ *
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ */
 class LargestPalindromeProductSpec extends ObjectBehavior
 {
     function it_finds_largest_palindrome_for_2_3_digit_numbers()
