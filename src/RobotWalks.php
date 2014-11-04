5520e19f52886a3512982faf0ecb79029f854a0f
Dimitrij P <d.phoursa@outlook.com>
2014-11-04 09:35:17 +0000

    Added comment describing a problem RobotWalks class is trying to solve

--- src/RobotWalks.php
+++ src/RobotWalks.php
@@ -6,6 +6,18 @@ print $robot->getClosedPathsCount() . PHP_EOL;
 $robot = new RobotWalks(70);
 print $robot->getClosedPathsCount() . PHP_EOL;
 
+/**
+ *
+ * Problem 208
+ * 
+ * A robot moves in a series of one-fifth circular arcs (72¡), with a free choice of a clockwise or an anticlockwise arc for each step, but no turning on the spot.
+ * 
+ * Ex: 25 arcs starting northward has 70932 possible closed paths
+ * 
+ * Given that the robot starts facing North, how many journeys of 70 arcs in length can it take that return it, after the final arc, to its starting position?
+ * (Any arc may be traversed multiple times.)
+ *
+ */
 class RobotWalks
 {
     private $arcs;
