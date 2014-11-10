f63641eb255395ad5f87888a3013fbba50c0a379
Dimitrij P <d.phoursa@outlook.com>
2014-11-10 14:54:52 +0000

    Added solution to Euler's 24th problem

--- spec/LexicographicPermutationsSpec.php
+++ spec/LexicographicPermutationsSpec.php
@@ -23,8 +23,38 @@ use Prophecy\Argument;
  */
 class LexicographicPermutationsSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_returns_1st_permutation_of_012()
     {
-        $this->shouldHaveType('LexicographicPermutations');
+        $this->findPermutation([0, 1, 2], 1)->shouldBe([0, 1, 2]);
+    }
+    
+    function it_returns_2nd_permutation_of_012()
+    {
+        $this->findPermutation([0, 1, 2], 2)->shouldReturn([0, 2, 1]);
+    }
+    
+    function it_returns_3rd_permutation_of_012()
+    {
+        $this->findPermutation([0, 1, 2], 3)->shouldReturn([1, 0 ,2]);
+    }
+    
+    function it_returns_4th_permutation_of_012()
+    {
+        $this->findPermutation([0, 1, 2], 4)->shouldReturn([1, 2, 0]);
+    }
+    
+    function it_returns_5th_permutation_of_012()
+    {
+        $this->findPermutation([0, 1, 2], 5)->shouldReturn([2, 0, 1]);
+    }
+    
+    function it_returns_6th_permutation_of_012()
+    {
+        $this->findPermutation([0, 1, 2], 6)->shouldReturn([2, 1, 0]);
+    }
+    
+    function it_returns_1000000th_permutation_of_0123456789()
+    {
+        $this->findPermutation([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 1000000)->shouldReturn([2, 7, 8, 3, 9, 1, 5, 4, 6, 0]);
     }
 }
--- src/LexicographicPermutations.php
+++ src/LexicographicPermutations.php
@@ -2,4 +2,39 @@
 
 class LexicographicPermutations
 {
+    public function findPermutation($numbers, $position)
+    {
+        if ($position == 1) {
+            return $numbers;
+        }
+        
+        for ($i = 1; $i < $position; $i++) {
+            $numbers = $this->permutate($numbers);
+        }
+
+        return $numbers;
+    }
+    
+    private function permutate($numbers)
+    {
+        //1. Find largest index i such that array[i - 1] < array[i].
+        $i = count($numbers) - 1;
+        while (isset($numbers[$i - 1]) && $numbers[$i - 1] >= $numbers[$i]) {
+            $i--;
+        }
+
+        //2. Find largest index j such that j ³ i and array[j] > array[i - 1].
+        $j = count($numbers) - 1;
+        while ($numbers[$j] <= $numbers[$i - 1]) {
+            $j--;
+        }
+        
+        //3. Swap array[j] and array[i - 1].
+        $tmp = $numbers[$i - 1];
+        $numbers[$i - 1] = $numbers[$j];
+        $numbers[$j] = $tmp;
+
+        //4. Reverse the suffix starting at array[i].
+        return array_merge(array_slice($numbers, 0, $i), array_reverse(array_slice($numbers, $i), 1));
+    }
 }
