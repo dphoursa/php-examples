adeef83a93b04701ed9ccc27dfeeefeb9762e654
Dimitrij P <d.phoursa@outlook.com>
2014-12-01 11:58:13 +0000

    Added comments

--- src/LargestPrimeFactor.php
+++ src/LargestPrimeFactor.php
@@ -1,7 +1,24 @@
 <?php
 
+/**
+ *
+ * Largest prime factor
+ *
+ * Problem 3
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ *
+ */
 class LargestPrimeFactor
 {
+    
+    /**
+     * Find largest prime factore of a number
+     *
+     * @param int $number Number to find a largest prime for
+     * @return int Largest prime
+     */
     public function compute($number)
     {
         $largest = 0;
